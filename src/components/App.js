4b00fa2d4f84ee48fe27fedfc9b65fcdbd0a1cff
wences627 <wences628@gmail.com>
2020-04-16 15:22:34 -0400

    haciendo pruebas

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header 
+        title= "hola"
+      />
+      <p>Aqui veremos que ocurre con las modificaciones</p>
+    </div>
+  );
+}
+
+export default App;
