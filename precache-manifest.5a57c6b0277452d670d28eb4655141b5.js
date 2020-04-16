f5216b49d2bdc40b28711a04b63fc24ed69397ec
wences627 <wences628@gmail.com>
2020-04-16 15:07:11 -0400

    Updates

---
+++ precache-manifest.5a57c6b0277452d670d28eb4655141b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a84cedf20d4b2e0e9934b95229c322e",
+    "url": "/covid-deploy/index.html"
+  },
+  {
+    "revision": "c4fcc122a2b6cdd12d07",
+    "url": "/covid-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "daacffada883954a6ab4",
+    "url": "/covid-deploy/static/js/2.b2276276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-deploy/static/js/2.b2276276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fcc122a2b6cdd12d07",
+    "url": "/covid-deploy/static/js/main.136412e9.chunk.js"
+  },
+  {
+    "revision": "35dc31549ece50c8a145",
+    "url": "/covid-deploy/static/js/runtime-main.f605e9bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
